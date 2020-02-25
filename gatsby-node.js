640c03c4020745feaa6796d83def45346e359563
jgrenat <jgrenat@users.noreply.github.com>
2020-02-25 10:31:58 +0100

    Remove unneeded logs

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,75 +1,82 @@
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
-
+const path = require(`path`);
+const { createFilePath } = require(`gatsby-source-filesystem`);
 
 function buildArticles(graphql, createPage) {
   return new Promise((resolve, reject) => {
     graphql(`
-{
-  allDatoCmsArticle {
-    edges {
-      node {
-        _allTitleLocales {
-          locale
-          value
+      {
+        allDatoCmsArticle {
+          edges {
+            node {
+              _allTitleLocales {
+                locale
+                value
+              }
+              title
+              slug
+            }
+          }
         }
-        title
-        slug
-  }}}
-}`).then(result => {
-      result.data.allDatoCmsArticle.edges.map(({node: article}) => {
+      }
+    `).then(result => {
+      result.data.allDatoCmsArticle.edges.map(({ node: article }) => {
         createPage({
           path: `article/${article.slug}`,
           component: path.resolve(`./src/templates/article.js`),
           context: {
-            slug: article.slug,
-          },
+            slug: article.slug
+          }
         });
       });
-      resolve()
-    })
-  })
+      resolve();
+    });
+  });
 }
 
-
-function buildPress(graphql, createPage)  {
-    return new Promise((resolve, reject) => {
-        graphql(`
-{
-  allDatoCmsArticle(filter: {articleType: {in: ["press", "interview"]}}) {
-    edges {
-      node {
-        _allTitleLocales {
-          locale
-          value
+function buildPress(graphql, createPage) {
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allDatoCmsArticle(
+          filter: { articleType: { in: ["press", "interview"] } }
+        ) {
+          edges {
+            node {
+              _allTitleLocales {
+                locale
+                value
+              }
+              title
+              slug
+            }
+          }
         }
-        title
-        slug
-  }}}
-}`).then(result => {
-            console.log('carotte');
-            console.log(result.data.allDatoCmsArticle.edges);
-            result.data.allDatoCmsArticle.edges.map(({node: article}) => {
-                console.log(article);
-                console.log("\n\n--------------\n\n");
-                createPage({
-                    path: `press/${article.slug}`,
-                    component: path.resolve(`./src/templates/press.js`),
-                    context: {
-                        slug: article.slug,
-                    },
-                });
-            });
-            resolve()
-        }, err => console.log('Error carotte', err))
-    });
+      }
+    `).then(
+      result => {
+        result.data.allDatoCmsArticle.edges.map(({ node: article }) => {
+          console.log(article);
+          console.log("\n\n--------------\n\n");
+          createPage({
+            path: `press/${article.slug}`,
+            component: path.resolve(`./src/templates/press.js`),
+            context: {
+              slug: article.slug
+            }
+          });
+        });
+        resolve();
+      },
+      err => console.log("Error carotte", err)
+    );
+  });
 }
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
 
   return Promise.all([
-      buildArticles(graphql, createPage),
-      buildPress(graphql, createPage)
+    buildArticles(graphql, createPage),
+    buildPress(graphql, createPage)
   ]);
 };
